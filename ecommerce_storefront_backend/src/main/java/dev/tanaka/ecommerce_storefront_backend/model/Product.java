f37e9fe240d677d66a5b6878662bdbd5e00c4fe9
TanakaZuze <99540354+TanakaZuze@users.noreply.github.com>
2025-10-03 17:29:00 +0200

    Update Product.java

--- ecommerce_storefront_backend/src/main/java/dev/tanaka/ecommerce_storefront_backend/model/Product.java
+++ ecommerce_storefront_backend/src/main/java/dev/tanaka/ecommerce_storefront_backend/model/Product.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 
 @Entity
+@Data
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,49 +15,6 @@ public class Product {
     private double product_price;
     private String product_image_url;
 
-
-    public Product() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProduct_name() {
-        return product_name;
-    }
-
-    public void setProduct_name(String product_name) {
-        this.product_name = product_name;
-    }
-
-    public String getProduct_description() {
-        return product_description;
-    }
-
-    public void setProduct_description(String product_description) {
-        this.product_description = product_description;
-    }
-
-    public double getProduct_price() {
-        return product_price;
-    }
-
-    public void setProduct_price(double product_price) {
-        this.product_price = product_price;
-    }
-
-    public String getProduct_image_url() {
-        return product_image_url;
-    }
-
-    public void setProduct_image_url(String product_image_url) {
-        this.product_image_url = product_image_url;
-    }
 
 
 }
